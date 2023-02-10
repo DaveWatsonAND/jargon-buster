750425b87e8884d45f1353a06d9d3c1f69075851
David Watson <davidwatson@CQ2R32N6R9.local>
2023-02-10 15:04:55 +0000

    added jargon buster func

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
+<p v-html="jargonBustedString"></p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  mounted(){
+    this.description.forEach((word)=>{
+      return document.querySelectorAll(`[id=${word.id}]`).forEach((ele)=>ele.addEventListener('click',this.clickTest))
+    })
+  },
+
+  methods:{
+    clickTest(event){
+        this.activeWord = (this.description.find((val)=>val.id == [event.target.id])).description
+        alert(this.activeWord)
+    }
+  },
+  
+  data() {
+  return {
+      text:"This is a test which includes a test-case for the demo, this is a sentence",
+      description:[
+          {word:"test",description:"this is a test description for the word test", id:"testid"},
+          {word:"test-case",description:"this is a is description for the word test case", id:"testCaseId"},
+          {word:"this is a sentence",description:"this is a is description for the test sentence",id:"sentenceId"}
+          ],
+      activeWord:'',
+}
+
+},
+computed:{
+jargonBustedString(){
+  this.description.map(word=>{
+      this.text =this.text.replace(word.word,`<a id=${word.id} id="tooltip-button">${word.word}</a>`)
+      console.log(this.text)
+    })
+
+    return this.text
+}
+
+}
 }
 </script>
 
@@ -22,5 +58,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+a{
+  font-weight: bold;
+  text-decoration: underline;
 }
 </style>
